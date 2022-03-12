142dda261979cf52d5226d24d58382976975505f
Andrew Lara <aandrewlaraa@gmail.com>
2022-03-12 05:19:23 -0500

    lets see

--- Seeplusplus.cpp
+++ Seeplusplus.cpp
@@ -8,16 +8,18 @@ using namespace std;
 class Person {
     public:
         int getAge(){return age;}
-        string getSex(){return (age == 0) ? "male" : "female";}
+        string getSex(){return (age == 0) ? "female" : "male";}
         string ReturnFullFamilyLine(){
             stringstream str;
-            str << familyLine.at(0).ToString() << " <-- dad ::: mom --> " << familyLine.at(1).ToString();
-
+            str << familyLine.at(0).ToString() << " <-- dad ::: mom --> " << familyLine.at(1).ToString() << endl;
+            str << name << endl;
+            return str.str();
         }
         string ToString(){ return name;}
+    
     private: 
         unsigned int age;
         string name;
         vector<Person> familyLine;
-        unsigned int sex;
+        unsigned int sex; // 1 is male 0 is female
 };
