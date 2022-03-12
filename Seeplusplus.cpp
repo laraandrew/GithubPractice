df4d433b06fe8c9839f1a4cb35613de9233d9922
Andrew Lara <aandrewlaraa@gmail.com>
2022-03-12 03:53:03 -0500

    push

---
+++ Seeplusplus.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Person {
+    public:
+        int getAge(){return age;}
+        string getSex(){return (age == 0) ? "male" : "female";}
+    private: 
+        unsigned int age;
+        string name;
+        vector<Person> familyLine;
+        unsigned int sex;
+};
